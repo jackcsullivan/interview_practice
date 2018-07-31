ccab34b10d28ac4811a6e8b224157cda2a9fab97
Jack Sullivan <jsullivan27@berkeley.edu>
2018-07-30 17:38:57 -0700

    dfs/bfs to Binary Tree

--- data_structures_imps/BT.java
+++ data_structures_imps/BT.java
@@ -1,4 +1,6 @@
 import java.lang.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 /* Inspired by https://www.sanfoundry.com/java-program-implement-binary-tree/ 
    NOT A BST (aka no logical order, just a binary tree). */
@@ -108,6 +110,41 @@ public class BT {
 		return (1 + Math.max(this.height(r.getLeft()), this.height(r.getRight())));
 	}
 
+	public void searchDFS(int d) {
+		this.searchDFS(this.root, d);
+	}
+
+	private void searchDFS(BTNode r, int d) {
+		if (r == null) {
+			return;
+		}
+		if (r.getData() == d) {
+			System.out.println("DFS Search successful!");
+		}
+		this.searchDFS(r.getLeft(), d);
+		this.searchDFS(r.getRight(), d);
+	}
+
+	public void searchBFS(int d) {
+		Queue<BTNode> q = new LinkedList<BTNode>();
+		q = this.updateQueue(this.root, q);
+		while (!q.isEmpty()) {
+			BTNode r = q.poll();
+			if (r.getData() == d) {
+				System.out.println("BFS Search successful!");
+			}
+			q = this.updateQueue(r.getLeft(), q);
+			q = this.updateQueue(r.getRight(), q);
+		}
+	}
+
+	private Queue updateQueue(BTNode r, Queue q) {
+		if (r != null) {
+			q.offer(r);
+		}
+		return q;
+	}
+
 	public static void main(String[] args) {
 		/* Simple sanity checks. */
 		BT bt = new BT();
@@ -119,5 +156,7 @@ public class BT {
 		bt.inorder();
 		System.out.println(bt.countNodes());
 		System.out.println(bt.height());
+		bt.searchDFS(20);
+		bt.searchBFS(3);
 	}
 }
